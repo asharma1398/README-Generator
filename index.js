bf4c11f88aa96b4bdc936d5e5d80a084a36e195f
asharma1398 <asharma1398@gmail.com>
2020-06-27 12:01:22 -0400

    title successfully generated to new readme

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
+// establishing requirements 
+var inquirer = require("inquirer");
+var fs = require("fs");
+var generateMarkdown = require("./utils/generateMarkdown.js");
+
+// name of new generated README file 
+var newFile = "generatedREADME.md"
+
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        message: "What is your project's name?",
+        name: "title"
+    }
 
 ];
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
-
-// function to initialize program
+// function to write README file & initialize program
 function init() {
-
+    inquirer
+    .prompt(questions)
+    .then(function(response) {
+        
+        fs.writeFile(newFile, generateMarkdown(response), function(err) {
+            if (err) throw err;
+            else console.log(`Success! Your new file is called ${newFile}`);
+        })
+    })
 }
 
 // function call to initialize program
