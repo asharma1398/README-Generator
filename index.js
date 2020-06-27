65645e7c8444e998188da97f8c6e6633fef23b54
asharma1398 <asharma1398@gmail.com>
2020-06-27 13:44:13 -0400

    question array populated

--- index.js
+++ index.js
@@ -12,6 +12,49 @@ const questions = [
         type: "input",
         message: "What is your project's name?",
         name: "title"
+    },
+    {
+        type: "input",
+        message: "Please provide a project description:",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    },
+    {
+        type: "list",
+        message: "What kind of license does your project have?",
+        name: "license",
+        choices: ["MIT", "Apache 2.0", "GPL 3.0", "BSD 3", "NONE"]
+    },
+    {
+        type: "input",
+        message: "What command should be run to install dependencies?",
+        default: "npm i",
+        name: "install"
+    },
+    {
+        type: "input",
+        message: "What command should be run to run tests?",
+        default: "npm test",
+        name: "test"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about using the repo?",
+        name: "using"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about contributing the repo?",
+        name: "contributing"
     }
 
 ];
