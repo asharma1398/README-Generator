09386e171172921f23855b16326faae8144d2b8f
asharma1398 <asharma1398@gmail.com>
2020-06-27 15:08:15 -0400

    badge license added to new readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,15 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
+  
+  // created compatible license name to later join to badge URL
+  var printLicense = data.license.split(" ").join("_")
+ 
   return `# ${data.title}
 
+  ![GitHub License](https://img.shields.io/badge/license-${printLicense}-blue.svg)
+
 `;
+
 }
 
 module.exports = generateMarkdown;
