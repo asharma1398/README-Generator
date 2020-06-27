4c83c3d00241ecb26149736649f8605d94e01c8d
asharma1398 <asharma1398@gmail.com>
2020-06-27 17:43:34 -0400

    table of contents items added to new readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -26,6 +26,36 @@ function generateMarkdown(data) {
 
   * [Questions](#questions)
 
+  ## Installation
+
+  To install necessary dependencies, run the following command: 
+
+    ${data.install}
+
+  ## Usage
+
+  ${data.using}
+
+  ## License 
+
+  This project is licensed under the ${data.license} license
+
+  ## Contributing 
+
+  ${data.contributing}
+
+  ## Tests
+
+  To run tests, run the following command:
+
+    ${data.test}
+
+  ## Questions 
+
+  If you have any questions about the repo, open an issue or contact me directly at [${data.email}](mailto:${data.email}).
+
+  You can find more of my work at [${data.username}](https://github.com/${data.username})
+
 `;
 
 }
